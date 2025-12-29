b51eff43187fd04a29ee7953e14b16175d82fdb0
athospozo <athospozoo@gmail.com>
2025-12-29 15:10:40 -0300

    fix: consertando exercicio 8

--- 8.js
+++ 8.js
@@ -3,7 +3,7 @@ function countBooksInCategory (array) {
     //criacao do nosso objeto:
     let objeto = {};
 
-    //percorrendo nosso array:
+    //percorrendo nosso array e preeenchendo o objeto:
     for (let i = 0; i < array.length; i++){
         objeto[array[i].category] = array[i].books.length;
     }
@@ -11,86 +11,3 @@ function countBooksInCategory (array) {
     //retornando o objeto:
     return objeto;
 }
-
-
-const booksByCategory = [
-  {
-    category: "Romance",
-    books: [
-      {
-        title: "O Sol Também é Uma Estrela",
-        author: "Nicola Yoon",
-      },
-      {
-        title: "O Noivo da Minha Melhor Amiga",
-        author: "Emily Giffin",
-      },
-      {
-        title: "Orgulho e Preconceito",
-        author: "Jane Austen",
-      },
-    ],
-  },
-  {
-    category: "Fantasia",
-    books: [
-      {
-        title: "Harry Potter e a Pedra Filosofal",
-        author: "J.K. Rowling",
-      },
-      {
-        title: "O Hobbit",
-        author: "J.R.R. Tolkien",
-      },
-      {
-        title: "Eragon",
-        author: "Christopher Paolini",
-      },
-      {
-        title: "O Nome do Vento",
-        author: "Patrick Rothfuss",
-      },
-    ],
-  },
-  {
-    category: "Suspense",
-    books: [
-      {
-        title: "A Garota no Trem",
-        author: "Paula Hawkins",
-      },
-      {
-        title: "O Silêncio dos Inocentes",
-        author: "Thomas Harris",
-      },
-    ],
-  },
-  {
-    category: "Ficção Científica",
-    books: [
-      {
-        title: "Duna",
-        author: "Frank Herbert",
-      },
-      {
-        title: "Neuromancer",
-        author: "William Gibson",
-      },
-      {
-        title: "Fundação",
-        author: "Isaac Asimov",
-      },
-      {
-        title: "1984",
-        author: "George Orwell",
-      },
-      {
-        title: "O Fim da Infância",
-        author: "Arthur C. Clarke",
-      },
-    ],
-  },
-];
-
-let objeto = countBooksInCategory (booksByCategory);
-console.log(objeto);
