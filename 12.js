a9a4e1aecb099b4547004002b371981403b81d81
athospozo <athospozoo@gmail.com>
2026-01-02 16:52:26 -0300

    feat: exercicio 12 concluido

---
+++ 12.js
@@ -0,0 +1,47 @@
+//gerando funcao:
+function encontrarElementoUnico (array) {
+
+    //gerando um array de analise:
+    const arrayObjetos = [];
+
+    //percorrendo o array de parametro:
+    for (let numero of array){
+
+        //definindo mais variaveis:
+        let achou = false; 
+        let aux = {};
+
+        //buscando o numero, caso o nosso array de armazenamento seja nulo pula:
+        for (let i = 0; i < arrayObjetos.length; i++) {
+
+            //checando se o elemento ja esta armazenado no array:
+            if (arrayObjetos[i].elemento == numero) {
+                arrayObjetos[i].vezes++; 
+                achou = true;       
+                break;           
+            }
+        }
+
+        //caso o elemento ainda nao esteja armazenado no nosso array:
+        if (!achou) {
+            aux.elemento = numero;
+            aux.vezes = 1;
+            arrayObjetos.push(aux);
+        }
+    }
+
+    //agora vamos percorrer o array objeto procurando o elemento que aparece apenas uma unica vez:
+    for (let i = 0; i < arrayObjetos.length; i++){
+
+        if (arrayObjetos[i].vezes == 1) return arrayObjetos[i].elemento;
+    }
+
+    console.log (encontrarElementoUnico(array));
+}
+
+let array = []
+for (let i = 0; i < 5; i++){
+    array[i] = Number (prompt ("digite o numero"));
+}
+
+console.log (encontrarElementoUnico(array));
